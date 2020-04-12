e8ff185181f211f6b2dd61d746aa8718cf6577a3
trocoli96 <Moreno54->
2020-04-12 13:25:36 +0200

    Map styling and starting with markers

--- frontend/guideo-app/components/HomeScreen.js
+++ frontend/guideo-app/components/HomeScreen.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, {useEffect, useState} from 'react';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import {mapStyle} from '../Styles/MapStyle';
+import * as Location from "expo-location";
+import {watchPositionAsync} from "expo-location";
+import MarkerImage from '../marker.png';
 
 function HomeScreen({navigation}) {
 
@@ -17,16 +21,32 @@ function HomeScreen({navigation}) {
         },
     });
 
+
+
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <View style={styles.container}>
                 <MapView style={styles.mapStyle}
+                         provider={PROVIDER_GOOGLE}
+                         customMapStyle={mapStyle}
                          initialRegion={{
                              latitude: 41.3959454,
                              longitude: 2.17863,
                              latitudeDelta: 0.0922,
                              longitudeDelta: 0.0421,
-                         }}/>
+                         }}>
+                    <Marker
+                        coordinate={
+                            {
+                                latitude: 41.3959454,
+                                longitude: 2.17863
+                            }
+                        }
+                        title={"Pussy"}
+                        description={"description"}
+                        image={MarkerImage}
+                    />
+                </MapView>
             </View>
         </View>
     );
--- frontend/guideo-app/components/ListLocationsScreen.js
+++ frontend/guideo-app/components/ListLocationsScreen.js
@@ -1,13 +1,14 @@
 import {Text, View} from "react-native";
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import * as Location from 'expo-location';
 
 function ListLocationsScreen() {
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>List of Lbocations</Text>
+            <Text>List of Locations</Text>
         </View>
-    );
+        );
 }
 
 export default ListLocationsScreen;
