7c2559d79a58d95fdd71bbbe90a915e6b94a4375
trocoli96 <Moreno54->
2020-04-12 13:27:10 +0200

    Mapview changed

--- frontend/guideo-app/components/HomeScreen.js
+++ frontend/guideo-app/components/HomeScreen.js
@@ -24,31 +24,27 @@ function HomeScreen({navigation}) {
 
 
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <View style={styles.container}>
-                <MapView style={styles.mapStyle}
-                         provider={PROVIDER_GOOGLE}
-                         customMapStyle={mapStyle}
-                         initialRegion={{
-                             latitude: 41.3959454,
-                             longitude: 2.17863,
-                             latitudeDelta: 0.0922,
-                             longitudeDelta: 0.0421,
-                         }}>
-                    <Marker
-                        coordinate={
-                            {
-                                latitude: 41.3959454,
-                                longitude: 2.17863
-                            }
+        <MapView style={styles.mapStyle}
+                 provider={PROVIDER_GOOGLE}
+                 customMapStyle={mapStyle}
+                 initialRegion={{
+                     latitude: 41.3959454,
+                     longitude: 2.17863,
+                     latitudeDelta: 0.0922,
+                     longitudeDelta: 0.0421,
+                 }}>
+                <Marker
+                    coordinate={
+                        {
+                            latitude: 41.3959454,
+                            longitude: 2.17863
                         }
-                        title={"Pussy"}
-                        description={"description"}
-                        image={MarkerImage}
-                    />
-                </MapView>
-            </View>
-        </View>
+                    }
+                    title={"Pussy"}
+                    description={"description"}
+                    image={MarkerImage}
+                />
+        </MapView>
     );
 }
 
