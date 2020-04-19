52a8fec4483ffa8b6ab87781b125342879d10c32
Alfonso <alfonso.trocoli@gmail.com>
2020-04-19 21:41:39 +0200

    Eliminado estilo innecesario y puesto como ruta inicial Home

--- frontend/guideo-app/App.js
+++ frontend/guideo-app/App.js
@@ -18,7 +18,7 @@ export default function App({navigation}) {
     return (
         <NavigationContainer>
             <Tab.Navigator
-                
+                initialRouteName={'Home'}
                 screenOptions={({ route }) => ({
                     tabBarIcon: ({ focused, color, size }) => {
                         let iconName;
@@ -45,7 +45,7 @@ export default function App({navigation}) {
                     showLabel: false,
                     style: {
                         borderTopColor: '#E0E0E0',
-                        borderTopWidth: '1px' 
+                        borderTopWidth: 1 
                     }
                 }}
             >
@@ -58,20 +58,5 @@ export default function App({navigation}) {
         </NavigationContainer>
     );
 
-}
-
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    logo:{
-        width: '80%',
-        height:'50%'
-    }
-});
+};
 
