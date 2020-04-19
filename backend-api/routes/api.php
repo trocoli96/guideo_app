32a87765c4086e035879cef1e45ce7556344ca51
Alfonso Trocoli <alfonso.trocoli@gmail.com>
2020-04-19 13:25:33 +0200

    Update api.php

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -37,6 +37,6 @@ Route::put('/profilepicture', 'AuthController@updateProfilePic');
  */
 
 Route::post('poi/create', 'PoiController@createPoi');
-Route::post('location/create', 'PoiController@createLocation');
-Route::get('locations/find', 'PoiController@getLocations');
 Route::get('poi/{id}', 'PoiController@getPoiById');
+Route::post('locations', 'PoiController@createLocation');
+Route::get('locations', 'PoiController@getLocations');
